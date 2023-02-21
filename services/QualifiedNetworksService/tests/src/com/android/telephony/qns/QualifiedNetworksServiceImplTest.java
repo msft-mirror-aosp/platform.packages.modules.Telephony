3fb7100cb4ddc6d787cc3a54c9f2ad7ca094d83c
sangyun <sangyun@google.com>
2023-02-22 06:29:06 +0900

    [QNS] Introduction of the qns metric (QNS metric v1.3)
    Atom for QualifiedRatListChanged
     - Pushed atom when a QNS update qualified RAT list.
    Atom for FallbackRestrictionChanged
     - Pushed atom when QNS fallback restriction changed.
    Atom for ImsCallDrop
     - Pushed atom when IMS call drop occurred in when both transport
       type available.
    Atom for RatPreferenceMismatchInfo
     - Pulls the history for mismatch between RAT preference and
       actual one.
    Atom for HandoverTimeMillis
     - Pulls for time taken from update of RAT preference until
       successful handover completion.
    Atom for HandoverPingpong
     - Pulls for count of handover pingpong in short time
    
    Bug: 235436630
    Test: atest QualifiedNetworksServiceTests
    Change-Id: I5b3bd2db6c1ca00412e0cbb2a7435fde7bc28015

--- services/QualifiedNetworksService/tests/src/com/android/telephony/qns/QualifiedNetworksServiceImplTest.java
+++ services/QualifiedNetworksService/tests/src/com/android/telephony/qns/QualifiedNetworksServiceImplTest.java
@@ -93,6 +93,7 @@ public class QualifiedNetworksServiceImplTest extends QnsTest {
                     mMockQnsCallStatusTracker,
                     mMockWifiBm,
                     mMockWifiQm,
+                    mMockQnsMetrics,
                     slotId);
         }
 
